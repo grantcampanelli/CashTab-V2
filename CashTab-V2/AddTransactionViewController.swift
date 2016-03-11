172c026de72d9a6962eaa2255870d114e7ad318a
Grant Campanelli <grantcampanelli@gmail.com>
2016-03-10 16:29:51 -0800

    Adding all files

---
+++ CashTab-V2/AddTransactionViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AddTransactionViewController.swift
+//  CashTab-V2
+//
+//  Created by Grant Campanelli on 3/10/16.
+//  Copyright © 2016 Grant Campanelli. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddTranscationViewController: UIViewController, UITextFieldDelegate {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+    }
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var costTextField: UITextField!
+    @IBOutlet weak var costLabel: UILabel!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    
+}
